f6f30802ce3b59d763ae9eff48d7f5f462149a88
soroush.jj00 <soroush.jj00@gmail.com>
2025-06-12 00:54:05 +0200

    AddBaseEntity

--- AuthService.Domain/Entities/User.cs
+++ AuthService.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuthService.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,12 @@ using System.Threading.Tasks;
 
 namespace AuthService.Domain.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
-        public Guid Id { get; set; }
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PasswordHash { get; set; } = null!;
         public string Role { get; set; } = "User";
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
